9666a3f330741bad2909ab2590d05d2c85c2ba4f
resource11 <mcmahon@resource11.com>
2020-04-15 17:54:44 -0400

    adds react-testing-library support

---
+++ setup-test-env.js
@@ -0,0 +1,7 @@
+// from the using-jest example link at the bottom of the docs page
+// this was a new file, not included in original docs
+require(`@testing-library/jest-dom/extend-expect`)
+
+// from Marcy Sutton JS a11y workshop
+// require(`@testing-library/jest-dom/extend-expect`)
+// require(`@testing-library/react/cleanup-after-each`)
---
+++ src/components/heading.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const Heading = () => (
+  <h1 data-testid="hero-title">Gatsby is awesome!</h1>
+)
+
+export default Heading
